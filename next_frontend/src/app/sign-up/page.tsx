2e0b5819e00455cec9895d3ade01fcbabb6fbc23
amaanbakshi7 <amaanbakshi777@gmail.com>
2025-03-09 22:26:08 -0500

    Code would not run on Aruv'v computer. Needed to compile on my computer to find the bug. We had an import error.

--- next_frontend/src/app/sign-up/page.tsx
+++ next_frontend/src/app/sign-up/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import React, { useState } from "react";
-import { signup } from "@/lib/auth";
 import Link from "next/link";
 import { GoogleOAuthProvider, GoogleLogin } from "@react-oauth/google";
 
