9aed3cc51661d18d3e6483db7ba2633526d4d568
ethan-maniv <ethan.maniv@gmail.com>
2025-04-07 13:27:29 -0500

    Added load_user function to extensions.py

--- flask_app/app/extensions.py
+++ flask_app/app/extensions.py
@@ -3,3 +3,11 @@ from flask_login import LoginManager
 
 db = SQLAlchemy()
 login_manager = LoginManager()
+
+@login_manager.user_loader
+
+def load_user(id):
+
+    from app.models import User
+
+    return User.query.get(int(id))
